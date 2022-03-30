a2df2987a90e35a9536bde07facb46414a4beddc
aviaro <94477208+aviaro@users.noreply.github.com>
2022-03-30 15:45:56 +0300

    30/03/2022

--- models/account.js
+++ models/account.js
@@ -1,26 +1,43 @@
-
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 
-const accountSchema = new Schema ({
-    _id:mongoose.Schema.Types.ObjectId,
-    associateId:{type:mongoose.Schema.Types.ObjectId,ref:'Account'},
+const accountSchema = new Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    associateId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Account'
+    },
     firstname: String,
-    lastname:String,
+    lastname: String,
     password: String,
-    mobile:String,
-    email:String,
-    isbusiness:{type: Boolean, default:false},
-    createdAt:{Type:Date, default:Date.now},
-    isBusiness: {type: Boolean, default: false},
-    isApproved: {type: Boolean, default: false},
-    isLocked: {type: Boolean, default: false},
-    
+    mobile: String,
+    email: String,
+    isbusiness: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        Type: Date,
+        default: Date.now
+    },
+    isBusiness: {
+        type: Boolean,
+        default: false
+    },
+    isApproved: {
+        type: Boolean,
+        default: false
+    },
+    isLocked: {
+        type: Boolean,
+        default: false
+    },
+
 
 
 
 
 })
 
-module.exports = mongoose.model('Account',accountSchema)
+module.exports = mongoose.model('Account', accountSchema)
--- models/schedule.js
+++ models/schedule.js
@@ -1,14 +1,27 @@
 const mongoose = require('mongoose')
-const Schema  =mongoose.Schema;
+const Schema = mongoose.Schema;
 
 
 const scheduleSchema = new Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    storeid: {type: mongoose.Schema.types.ObjectId,ref: 'store'},
-    accountid: {type: mongoose.Schema.types.objectid,ref:'Account'},
-    serviceid: {type: mongoose.Schema.types.object,ref:'Service'},
-    service_provider: String,ref:'service',
+    storeid: {
+        type: mongoose.Schema.types.ObjectId,
+        ref: 'Store'
+    },
+    accountid: {
+        type: mongoose.Schema.types.objectid,
+        ref: 'Account'
+    },
+    serviceid: {
+        type: mongoose.Schema.types.object,
+        ref: 'Service'
+    },
+    service_provider_id: {
+        type: mongoose.Schema.types.object,
+        ref: 'Account'
+    },
     schedule_date: Date,
+
     fromhours: Number,
     tohours: Number,
     estimated_staytime: Number,
@@ -22,4 +35,4 @@ const scheduleSchema = new Schema({
 
 
 
-module.exports = mongoose.model('schedule',scheduleSchema)
+module.exports = mongoose.model('Schedule', scheduleSchema)
